f09286a525fb80afbba12558bf84b6bb9929eb93
RealWxs <56788520+RealWxs@users.noreply.github.com>
2023-03-25 22:28:50 +0800

    Update example.py

--- example.py
+++ example.py
@@ -2,6 +2,8 @@ from delegator import Delegator
 
 delegator = Delegator()
 
+
+# argument is the case name in the switch
 @delegator.delegate('drink')
 def drink(juice):
     print(f"drink {juice}")
@@ -17,7 +19,9 @@ def sleep():
 
 if __name__ == "__main__":
     delegator.switch("drink", "apple juice")
+    # in switch, first argument is the variable to be switched, others is argument the target function need     
     delegator.switch(2, food="pizza", dessert="ice cream")
     delegator.switch(None)
+    # in dispatch, return value is the function to be executed, and you can pass the argument like you're just using that function      
     delegator.dispatch("drink")("orange juice")
     
